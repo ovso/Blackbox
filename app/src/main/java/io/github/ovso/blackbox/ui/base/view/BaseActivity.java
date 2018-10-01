1a9f19a66e3f85d388786533ec2e634c5dc8f76c
jaeho <ovsoce@gmail.com>
2018-10-01 23:42:05 +0900

    Added banner ad

--- app/src/main/java/io/github/ovso/blackbox/ui/base/view/BaseActivity.java
+++ app/src/main/java/io/github/ovso/blackbox/ui/base/view/BaseActivity.java
@@ -4,16 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import dagger.android.support.DaggerAppCompatActivity;
 import io.github.ovso.blackbox.R;
+import io.github.ovso.blackbox.main.base.view.MyAdView;
 
 public abstract class BaseActivity extends DaggerAppCompatActivity {
   protected @BindView(R.id.toolbar) Toolbar toolbar;
   private Unbinder bind;
-
+  @BindView(R.id.ad_container) ViewGroup adContainer;
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(getLayoutResID());
@@ -21,6 +23,7 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayShowTitleEnabled(isTitle());
     onCreated(savedInstanceState);
+    adContainer.addView(MyAdView.getAdmobAdView(this));
   }
 
   protected abstract int getLayoutResID();
--- app/src/main/java/io/github/ovso/blackbox/ui/base/view/MyAdView.java
+++ app/src/main/java/io/github/ovso/blackbox/ui/base/view/MyAdView.java
@@ -12,7 +12,7 @@ public class MyAdView {
   public static AdView getAdmobAdView(Context context) {
     AdView adView = new AdView(context);
     adView.setAdSize(AdSize.SMART_BANNER);
-    adView.setAdUnitId(Security.ADMOB_UNIT_ID.getValue());
+    adView.setAdUnitId(Security.ADMOB_BANNER_UNIT_ID.getValue());
     AdRequest adRequest = new AdRequest.Builder().build();
     adView.loadAd(adRequest);
     return adView;
