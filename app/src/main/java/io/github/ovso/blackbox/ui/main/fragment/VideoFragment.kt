3ef9338ac00e192e2baf3ceab191266c2190b50b
jaeho <ovsoce@gmail.com>
2019-01-08 22:14:18 +0900

    versioning

--- app/src/main/java/io/github/ovso/blackbox/ui/main/fragment/VideoFragment.kt
+++ app/src/main/java/io/github/ovso/blackbox/ui/main/fragment/VideoFragment.kt
@@ -1,7 +1,6 @@
 package io.github.ovso.blackbox.ui.main.fragment
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
