3a1cd1f03e3865a13a186ed550cebb628b4afe74
jaeho <ovsoce@gmail.com>
2020-03-23 23:58:15 +0900

    Versioning AndroiJUnit4

--- app/src/androidTest/java/io/github/ovso/blackbox/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/github/ovso/blackbox/ExampleInstrumentedTest.java
@@ -2,6 +2,9 @@ package io.github.ovso.blackbox;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -12,12 +15,13 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class) public class ExampleInstrumentedTest {
-  @Test
-  public void useAppContext() throws Exception {
-    // Context of the app under test.
-    Context appContext = InstrumentationRegistry.getTargetContext();
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-    assertEquals("io.github.ovso.blackbox", appContext.getPackageName());
-  }
+        assertEquals("io.github.ovso.blackbox", appContext.getPackageName());
+    }
 }
