94219e395b45a4d97aac828547660f538f6aaea3
jaeho <ovsoce@gmail.com>
2021-04-11 16:40:24 +0900

    feat: Add interstitial ad on Splash

--- app/src/main/java/io/github/ovso/blackbox/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/github/ovso/blackbox/ui/splash/SplashActivity.kt
@@ -3,11 +3,35 @@ package io.github.ovso.blackbox.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.LoadAdError
+import io.github.ovso.blackbox.exts.loadInterstitial
 import io.github.ovso.blackbox.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+  private val interstitialAd by lazy { loadInterstitial() }
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    interstitialAd.adListener = object : AdListener() {
+
+      override fun onAdFailedToLoad(p0: LoadAdError?) {
+        super.onAdFailedToLoad(p0)
+        launchMain()
+      }
+
+      override fun onAdLoaded() {
+        super.onAdLoaded()
+        interstitialAd.show()
+      }
+
+      override fun onAdClosed() {
+        super.onAdClosed()
+        launchMain()
+      }
+    }
+  }
+
+  private fun launchMain() {
     Intent(this, MainActivity::class.java).apply {
       startActivity(this)
       finish()
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     }
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.1.0")
+    classpath("com.android.tools.build:gradle:4.2.0-rc01")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
     classpath("com.google.gms:google-services:4.3.4")
     classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
